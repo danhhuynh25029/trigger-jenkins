c73c43b11918789c65ca4bff98cdaf29cfd996f7
danhhnc <danhhnc@CSS-DanhHNC-M.local>
2023-12-25 15:18:47 +0700

    feat: init service

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/items", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "item",
+		})
+	})
+	r.Run(":8083")
+}
