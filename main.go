8ea8fbf0c754391dc61364cc247c2dbb645c3aeb
danhhnc <danhhnc@CSS-DanhHNC-M.local>
2023-12-26 01:33:49 +0700

    feat: update api product

--- main.go
+++ main.go
@@ -19,9 +19,19 @@ func main() {
 		})
 	})
 	r.GET("/products", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "products",
-		})
+		type product struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+		}
+		var list []product
+
+		for i := 0; i < 10; i++ {
+			list = append(list, product{
+				ID:   i,
+				Name: "clothes",
+			})
+		}
+		c.JSON(http.StatusOK, list)
 	})
 	r.Run()
 }
